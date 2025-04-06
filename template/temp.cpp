c231a783491485d9b182eeb80c33d86bb63f6b96
Carlos-UCH <carlosbrasil254@gmail.com>
2025-04-06 13:04:31 -0300

    template para programação competitiva

---
+++ template/temp.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define endl '\n'
+
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3fll;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+
+    ll n; cin >> n; 
+
+    if(400%n==0) cout << 400/n << endl;
+    else cout << -1 << endl;
+	
+	
+
+return 0;
+}
