abc0788db3e26e3435c2ba1f236d9ab9871ecc02
Carlos-UCH <carlosbrasil254@gmail.com>
2025-04-06 12:53:09 -0300

    subconjuntos

---
+++ backtraking/subconjunto.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool is_soluction(int i, int N) {return i == N;}
+
+void process_soluction(const vector<int>&xs){
+
+cout << "{";
+
+for(auto x: xs) cout << x << ' '; 
+
+cout << "}" << endl; 
+
+}
+
+vector<int> candidates(int i, const vector<int>&as){
+	return {as[i]};
+}
+
+void backtraking(vector<int>& xs, int i, int N, const vector<int>&as){
+	if(is_soluction(i, N))
+		process_soluction(xs); 
+	else{
+		auto cs = candidates(i, as); 
+
+		for(auto c: cs){
+
+		  backtraking(xs, i+1, N, as);
+
+		  xs.push_back(c); 
+		  backtraking(xs, i + 1, N, as);
+		  xs.pop_back(); 
+		}
+	}
+	
+
+}
+
+int main(){
+	vector<int> as {2,3,5,7,11}, xs; 
+	backtraking(xs, 0, (int)as.size(), as); 
+	return 0; 
+
+}
