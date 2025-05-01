c83db0dcf80e91ab5940a01003bd1ac7432a6aa7
Carlos-UCH <carlosbrasil254@gmail.com>
2025-05-01 16:14:46 -0300

    A - Hexadecimal's Numbers

---
+++ lista5/hexadecimal.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vpll vector<pair<ll, ll>>
+#define endl '\n'
+#define found(x, y) (x.find(y) != x.end()) //set - map - unordered_set - unordered_map
+#define debug(x) cout << #x << " = " << x << endl
+
+const int inf = 0x3f3f3f3f;
+const ll linf = 0x3f3f3f3f3f3f3f3fll;
+ll meuJeitoNinja = 0; 
+
+template<typename Container, typename T>
+bool fd(const T& value, const Container& container) {
+    return find(container.begin(), container.end(), value) != container.end();
+} 
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); 
+	
+
+    ll n; cin >> n; 
+
+    vector<string> binarios; 
+    string valor = "";
+    for(ll i = 1; i < 1024; ++i) {
+        ll temp = i; 
+        while(temp >= 1){
+            valor += to_string(temp%2); 
+            temp/=2;     
+        }
+        reverse(all(valor));
+        binarios.emplace_back(valor); 
+        valor = "";      
+    }
+
+    for(ll i = 0 ; i < binarios.size(); ++i)
+        if(stol(binarios[i]) <= n) meuJeitoNinja++; 
+
+    cout << meuJeitoNinja << endl; 
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
