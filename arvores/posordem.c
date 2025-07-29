1e538bf9c0f66b5099f62148aef3030f306e9b50
Carlos-UCH <carlosbrasil254@gmail.com>
2025-07-29 16:43:37 -0300

    add: posordem

---
+++ arvores/posordem.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct no{
+    int dado; 
+    struct no *esq, *dir; 
+}no; 
+
+
+typedef struct pilha{
+    no **dados;
+    int N, topo;  
+}pilha; 
+
+
+pilha *criar_pilha(int N){
+    pilha *p = malloc(sizeof(pilha)); 
+    p->dados = malloc(N*sizeof(no*));
+    p->N = N; 
+    p->topo = 0;
+    return p; 
+}
+
+void empilha(pilha *p, no *x){
+    if(p->topo == p->N){
+        p->dados = realloc(p->dados, 2*p->N*sizeof(no*));
+        p->N*=2;  
+    }
+
+    p->dados[p->topo] = x; 
+    p->topo++; 
+}
+
+
+no *desempilha(pilha *p){
+    p->topo--; 
+    return p->dados[p->topo]; 
+}   
+
+
+void pos_ordem(no *raiz){
+    if(raiz == NULL) return NULL;
+    pilha *p1 = criar_pilha(100); 
+    pilha *p2 = criar_pilha(100); 
+
+    empilha(p1, raiz); 
+
+    while(p1->topo != 0){
+        no *atual = desempilha(p1); 
+        empilha(p2, atual); 
+
+        if(atual->esq != NULL) empilha(p1, atual->esq);
+        if(atual->dir != NULL) empilha(p1, atual->dir);   
+    }
+
+
+    while(p2->topo != 0){
+        no *aux = desempilha(p2); 
+        printf("%d ", aux->dado); 
+    }
+}
